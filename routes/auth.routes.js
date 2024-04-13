4566f846ac88db300f475652a74bf6237da56a8d
moreira8125 <your_github_email>
2024-04-13 13:55:57 +0100

    test

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -18,7 +18,7 @@ const saltRounds = 10;
 
 // POST /auth/signup  - Creates a new user in the database
 router.post("/signup", (req, res, next) => {
-  const { email, password, name } = req.body;
+  const { email, password, name, username } = req.body;
 
   // Check if email or password or name are provided as empty strings
   if (email === "" || password === "" || name === "") {
@@ -58,7 +58,7 @@ router.post("/signup", (req, res, next) => {
 
       // Create the new user in the database
       // We return a pending promise, which allows us to chain another `then`
-      return User.create({ email, password: hashedPassword, name });
+      return User.create({ email, password: hashedPassword, name, username });
     })
     .then((createdUser) => {
       // Deconstruct the newly created user object to omit the password
@@ -98,10 +98,10 @@ router.post("/login", (req, res, next) => {
 
       if (passwordCorrect) {
         // Deconstruct the user object to omit the password
-        const { _id, email, name } = foundUser;
+        const { _id, email, name, username } = foundUser;
 
         // Create an object that will be set as the token payload
-        const payload = { _id, email, name };
+        const payload = { _id, email, name, username };
 
         // Create a JSON Web Token and sign it
         const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
--- routes/event.routes.js
+++ routes/event.routes.js
@@ -1,65 +1,64 @@
 const express = require("express");
 const router = express.Router();
-const Event = require('../models/Event.model');
+const Event = require("../models/Event.model");
 
-router.get('/events', (req,res)=>{
-    Event.find()
+router.get("/events", (req, res) => {
+  Event.find()
     .populate("participants")
-    .then((allEvents)=>{
-        res.json(allEvents)
+    .then((allEvents) => {
+      res.json(allEvents);
     })
-    .catch((err)=>{
-        res.status(400).json(err, "No events found.")
-        console.log(err)
-    })
+    .catch((err) => {
+      res.status(400).json(err, "No events found.");
+      console.log(err);
+    });
 });
 
-router.get('/events/:_id',(req,res)=>{
-    Event.findById(req.params._id)
-    .populate('participants')
-    .then((oneEvent)=>{
-        res.json(oneEvent)
+router.get("/events/:_id", (req, res) => {
+  Event.findById(req.params._id)
+    .populate("participants")
+    .then((oneEvent) => {
+      res.json(oneEvent);
     })
-    .catch((err)=>{
-        res.status(400).json(err, "No event found.")
-        console.log(err)
-    })
+    .catch((err) => {
+      res.status(400).json(err, "No event found.");
+      console.log(err);
+    });
 });
 
-router.post('/events', (req,res)=>{
-    Event.create(req.body)
-    .then((newEvent)=>{
-        res.json(newEvent)
+router.post("/events", (req, res) => {
+  Event.create(req.body)
+    .then((newEvent) => {
+      res.json(newEvent);
     })
-    .catch((err)=>{
-        res.status(400).json(err, "Unable to create event, fill out all compulsory fields.")
-        console.log(err)
-    })
+    .catch((err) => {
+      res
+        .status(400)
+        .json(err, "Unable to create event, fill out all compulsory fields.");
+      console.log(err);
+    });
 });
 
-router.put('/events/:_id', (req,res)=>{
-    Event.findByIdAndUpdate(req.params._id, req.body, {new:true})
-    .then((updatedEvent)=>{
-        res.json(updatedEvent)
+router.put("/events/:_id", (req, res) => {
+  Event.findByIdAndUpdate(req.params._id, req.body, { new: true })
+    .then((updatedEvent) => {
+      res.json(updatedEvent);
     })
-    .catch((err)=>{
-        res.status(400).json(err, "Unable to edit event")
-    })
+    .catch((err) => {
+      res.status(400).json(err, "Unable to edit event");
+    });
 });
 
-router.delete('/events/:_id', (req,res)=>{
-    Event.findByIdAndDelete(req.params.id)
-    .then((deletedEvent)=>{
-        res.json("Event successfully deleted")
-        console.log(deletedEvent)
+router.delete("/events/:_id", (req, res) => {
+  Event.findByIdAndDelete(req.params._id)
+    .then((deletedEvent) => {
+      res.json("Event successfully deleted");
+      console.log(deletedEvent);
     })
-    .catch((err)=>{
-        res.status(400).json(err, "Unable to delete event")
-        console.log(err)
-    })
-})
-
-
-
+    .catch((err) => {
+      res.status(400).json(err, "Unable to delete event");
+      console.log(err);
+    });
+});
 
 module.exports = router;
